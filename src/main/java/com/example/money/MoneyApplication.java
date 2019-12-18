284516887ed32168f31e249482314616fac9532b
yevhenii <yevheniiskoryi@gmail.com>
2019-12-18 15:23:08 +0200

    updated CommandLineRunner

--- src/main/java/com/example/money/MoneyApplication.java
+++ src/main/java/com/example/money/MoneyApplication.java
@@ -1,19 +1,16 @@
 package com.example.money;
 
+import com.example.money.dto.application.WastingData;
 import com.example.money.entity.Money;
 import com.example.money.entity.Person;
-import com.example.money.repository.MoneyRepository;
 import com.example.money.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -27,15 +24,20 @@ public class MoneyApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        final Person person = new Person("YEVHENII SKORYI", new ArrayList<>(),new ArrayList<>());
+        final Person person = new Person("YEVHENII SKORYI", new ArrayList<>(), new ArrayList<>());
 
         final Money money1 = new Money(
-                LocalDateTime.of(2019,11,1,0,0),
-                (long)1000
+                LocalDateTime.of(2019, 11, 1, 0, 0),
+                (long) 1000
+        );
+        final WastingData wasting = new WastingData(
+                LocalDateTime.of(2020, 3, 2, 0, 0),
+                1000L,
+                "description"
         );
 
-
         person.addMoney(money1);
+        person.addWasting(wasting);
         userRepository.save(person);
     }
 }
